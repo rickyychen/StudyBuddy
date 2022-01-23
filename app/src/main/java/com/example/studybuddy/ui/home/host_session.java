519059d60c87194c8177cb3548127111cf44cd10
Juan David Guerra <juandavidguerra1@gmail.com>
2022-01-23 01:21:48 -0500

    collecting data from button solution 1

--- app/src/main/java/com/example/studybuddy/ui/home/host_session.java
+++ app/src/main/java/com/example/studybuddy/ui/home/host_session.java
@@ -1,33 +1,36 @@
 package com.example.studybuddy.ui.home;
 
 import android.content.Intent;
-import android.view.View;
-import android.widget.*;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.studybuddy.MainActivity;
 import com.example.studybuddy.R;
+import com.example.studybuddy.betterdb.Event;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class host_session extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     String[] locations = {"McLennan", "Redpath", "McConell", "Trottier"};
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host_session);
-
-        final Button button = findViewById(R.id.button3);
-        button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(host_session.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
 
         Spinner spin = (Spinner) findViewById(R.id.spinner_location);
         spin.setOnItemSelectedListener(this);
@@ -37,10 +40,32 @@ public class host_session extends AppCompatActivity implements AdapterView.OnIte
         spin.setAdapter(aa);
 
         DatePicker datepicker = (DatePicker) findViewById(R.id.datePicker1);
+        int   day  = datepicker.getDayOfMonth();
+        int   month= datepicker.getMonth();
+        int   year = datepicker.getYear();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        String formatedDate = sdf.format(calendar.getTime());
+
         TimePicker timepicker = (TimePicker) findViewById(R.id.timePicker1);
+        int hour = timepicker.getHour();
+        int minute = timepicker.getMinute();
+
+        String time = hour + ":" + minute;
 
         EditText description = (EditText) findViewById(R.id.description);
         String descriptionValue = description.getText().toString();
+
+        final Button button = findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Event newEvent = new Event(eventId, formatedDate, time, descriptionValue);
+                Intent intent = new Intent(host_session.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
